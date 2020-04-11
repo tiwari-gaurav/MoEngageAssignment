6cb8431904521e950102272a53fbb8c4629e4beb
gaurav <gaurav@emedsim.com>
2020-04-11 14:16:58 +0530

    FCM implemented

---
+++ app/src/main/java/com/moengage/assignment/firebase/MyFirebaseMessagingService.java
@@ -0,0 +1,83 @@
+package com.moengage.assignment.firebase;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+import com.moengage.assignment.R;
+import com.moengage.assignment.view.MainActivity;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onNewToken(String s) {
+        super.onNewToken(s);
+        Log.e("NEW_TOKEN", s);
+    }
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        sendMyNotification(remoteMessage.getNotification().getBody());
+    }
+
+    private void sendMyNotification(String message) {
+
+        //On click of notification it redirect to this Activity
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
+        String NOTIFICATION_CHANNEL_ID = "News_channel";
+
+        long pattern[] = {0, 1000, 500, 1000};
+
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "Your Notifications",
+                    NotificationManager.IMPORTANCE_HIGH);
+
+            notificationChannel.setDescription("");
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.setVibrationPattern(pattern);
+            notificationChannel.enableVibration(true);
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+
+        // to diaplay notification in DND Mode
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = mNotificationManager.getNotificationChannel(NOTIFICATION_CHANNEL_ID);
+            channel.canBypassDnd();
+        }
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+
+        notificationBuilder.setAutoCancel(true)
+                .setColor(ContextCompat.getColor(this, R.color.colorAccent))
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText(message)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setAutoCancel(true)
+                .setContentIntent(pendingIntent);
+
+
+        mNotificationManager.notify(1000, notificationBuilder.build());
+    }
+
+}
